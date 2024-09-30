5c5120c1a2102e459ddca833953292fd6e5cbb95
Marc Battistello <marc.battistello@gmail.com>
2024-09-30 11:22:27 -0500

    Create solitaire.py

---
+++ solitaire.py
@@ -0,0 +1,36 @@
+
+
+#classes for use
+class Card:
+    None
+
+class Deck:
+    suits = []
+
+    
+    None
+
+class StockPile:   #pile you pull from
+    None
+
+class FoundationPile:  # 4 piles you try to build
+    None
+
+class Column:  #this is 7 columns of cards you try to build
+    None
+
+#represents the board including
+# stock and foundation piles, columns, and cards in play. 
+
+class Board:
+
+
+    def __init__( self ):
+        None
+
+    def reset():
+        None
+
+
+    def solve():
+        None
